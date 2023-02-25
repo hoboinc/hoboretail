3ca7a0bc14eeefaace82ea945a5494f9fa400c2c
jhonny16 <tinny.1609@gmail.com>
2023-02-25 17:59:16 -0500

    hoboretail v:16.0.1.7

--- usa_hoboretail/models/__init__.py
+++ usa_hoboretail/models/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import account_journal
-from . import sale_order
 from . import product_template
+from . import sale_order
+from . import sale_order_line
 from . import account_move_files
 from . import account_move
 from . import account_move_line
