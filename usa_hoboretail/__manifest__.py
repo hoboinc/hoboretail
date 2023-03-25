45820987b77ad4fa2278e09129dc23d290059696
jhonny16 <tinny.1609@gmail.com>
2023-03-25 10:50:42 -0500

    hoboretail v:16.0.2.1

--- usa_hoboretail/__manifest__.py
+++ usa_hoboretail/__manifest__.py
@@ -9,7 +9,7 @@
     'author': "Ing.Jhonny Mack Merino Samillán",
     'website': "www.hoboretail.com ",
     'category': 'Sale',
-    'version': '16.0.2.0',
+    'version': '16.0.2.1',
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'account'],
     # always loaded
